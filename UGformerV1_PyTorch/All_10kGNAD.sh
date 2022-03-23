1006946fdbef47b7b2308f2f2561a476333d1f8f
Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>
2022-03-23 18:28:14 -0300

    Add 10kGNAD Only_order results
    Signed-off-by: Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>

--- UGformerV1_PyTorch/All_10kGNAD.sh
+++ UGformerV1_PyTorch/All_10kGNAD.sh
@@ -62,38 +62,38 @@
 #echo "      > Started at"  $(date)
 #python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Default-Self-TAG-None --ff_hidden_size 1024 --fold_idx 9 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Default-Self-TAG-None_idx9 --batch_size 4 > 10kGNAD-DE-Default-Self-TAG-None_idx9.txt
 #echo "Finished working on dataset [ 10kGNAD-DE-Default-Self-TAG-None ]"
-echo ">>>>> WORKING WITH DATASET [ 10kGNAD-DE-Only-Order-TAG-None ] <<<<<"
-echo "  >>> Running IDX [ 0 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 0 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx0 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx0.txt
-echo "  >>> Running IDX [ 1 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 1 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx1 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx1.txt
-echo "  >>> Running IDX [ 2 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 2 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx2 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx2.txt
-echo "  >>> Running IDX [ 3 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 3 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx3 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx3.txt
-echo "  >>> Running IDX [ 4 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 4 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx4 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx4.txt
-echo "  >>> Running IDX [ 5 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 5 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx5 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx5.txt
-echo "  >>> Running IDX [ 6 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 6 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx6 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx6.txt
-echo "  >>> Running IDX [ 7 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 7 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx7 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx7.txt
-echo "  >>> Running IDX [ 8 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 8 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx8 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx8.txt
-echo "  >>> Running IDX [ 9 ]"
-echo "      > Started at"  $(date)
-python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 9 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx9 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx9.txt
-echo "Finished working on dataset [ 10kGNAD-DE-Only-Order-TAG-None ]"
+#echo ">>>>> WORKING WITH DATASET [ 10kGNAD-DE-Only-Order-TAG-None ] <<<<<"
+#echo "  >>> Running IDX [ 0 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 0 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx0 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx0.txt
+#echo "  >>> Running IDX [ 1 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 1 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx1 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx1.txt
+#echo "  >>> Running IDX [ 2 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 2 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx2 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx2.txt
+#echo "  >>> Running IDX [ 3 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 3 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx3 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx3.txt
+#echo "  >>> Running IDX [ 4 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 4 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx4 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx4.txt
+#echo "  >>> Running IDX [ 5 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 5 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx5 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx5.txt
+#echo "  >>> Running IDX [ 6 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 6 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx6 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx6.txt
+#echo "  >>> Running IDX [ 7 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 7 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx7 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx7.txt
+#echo "  >>> Running IDX [ 8 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 8 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx8 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx8.txt
+#echo "  >>> Running IDX [ 9 ]"
+#echo "      > Started at"  $(date)
+#python ./train_UGformerV1_Sup.py --dataset 10kGNAD-DE-Only-Order-TAG-None --ff_hidden_size 1024 --fold_idx 9 --num_epochs 30 --num_timesteps 1 --learning_rate 0.0001 --num_hidden_layers 2 --model_name 10kGNAD-DE-Only-Order-TAG-None_idx9 --batch_size 4 > 10kGNAD-DE-Only-Order-TAG-None_idx9.txt
+#echo "Finished working on dataset [ 10kGNAD-DE-Only-Order-TAG-None ]"
 echo ">>>>> WORKING WITH DATASET [ 10kGNAD-DE-Order-Circular-TAG-None ] <<<<<"
 echo "  >>> Running IDX [ 0 ]"
 echo "      > Started at"  $(date)
