bbcb35596a6342e033875079cb815757bf3ff7f0
Henrique V. Ehrenfried <h.v.ehrenfried@hotmail.com>
2023-02-07 17:32:31 -0300

    Add some changes to the Script to read the GermEval2018

--- UGformerV1_PyTorch/Consolidate_GermEval2018.py
+++ UGformerV1_PyTorch/Consolidate_GermEval2018.py
@@ -1,39 +1,27 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import duckdb
 
 
-FILE_LOCATION = "./"
+FILE_LOCATION = "./Logs/"
 HEADER = ["Epoch", "Time_in_sec", "Loss", "Accuracy", "F1_Score", "Mode", "Representation"]
 
-COARSE = [
-    "GermEval2018-Coarse-AT-TAG-None_output.log",
-    "GermEval2018-Coarse-BT-TAG-None_output.log",
-    "GermEval2018-Coarse-DTORMS-TAG-None_output.log",
-    "GermEval2018-Coarse-DTORM-TAG-None_output.log",
-    "GermEval2018-Coarse-DTORS-TAG-None_output.log",
-    "GermEval2018-Coarse-DTOR-TAG-None_output.log",
-    "GermEval2018-Coarse-DTOYS-TAG-None_output.log",
-    "GermEval2018-Coarse-DTOY-TAG-None_output.log",
-    "GermEval2018-Coarse-GOW-TAG-None_output.log",
-    "GermEval2018-Coarse-RBT-TAG-None_output.log",
-    "GermEval2018-Coarse-ROC-TAG-None_output.log",
-    "GermEval2018-Coarse-ROO-TAG-None_output.log"
-] 
-
-FINE = [
-    "GermEval2018-Fine-AT-TAG-None_output.log",
-    "GermEval2018-Fine-BT-TAG-None_output.log",
-    "GermEval2018-Fine-DTORMS-TAG-None_output.log",
-    "GermEval2018-Fine-DTORM-TAG-None_output.log",
-    "GermEval2018-Fine-DTORS-TAG-None_output.log",
-    "GermEval2018-Fine-DTOR-TAG-None_output.log",
-    "GermEval2018-Fine-DTOYS-TAG-None_output.log",
-    "GermEval2018-Fine-DTOY-TAG-None_output.log",
-    "GermEval2018-Fine-GOW-TAG-None_output.log",
-    "GermEval2018-Fine-RBT-TAG-None_output.log",
-    "GermEval2018-Fine-ROC-TAG-None_output.log",
-    "GermEval2018-Fine-ROO-TAG-None_output.log"
+REPRESENTATIONS = [
+    "AT",
+    "BT",
+    "DTORMS",
+    "DTORM",
+    "DTORS",
+    "DTOR",
+    "DTOYS",
+    "DTOY",
+    "GOW",
+    "RBT",
+    "ROC",
+    "ROO"
 ]
+
+MODES = ["Coarse", "Fine"]
 
 ALL = [
     "GermEval2018-Coarse-AT-TAG-None_output.log",
@@ -65,7 +53,7 @@ ALL = [
 TEST = ["output_test.log", "output_test2.log"]
 
 def get_content(file):
-    with open(file, "r") as f:
+    with open(FILE_LOCATION+file, "r") as f:
         content = f.read()
     return content
 
@@ -81,19 +69,57 @@ def statistics(df):
     # TODO: Ceate queries to explore the data
     queries = [
         # Get total summary of each experiment
-         "SELECT COUNT(Epoch) as Num_epochs, Mode, Representation, SUM(Time_in_sec) AS Total_Time, MAX(Accuracy) AS Max_ACC, MAX(F1_Score) AS Max_F1 FROM df GROUP BY Mode, Representation ORDER BY Mode, Representation, Max_F1 DESC, Max_ACC DESC"     
-       
+        "SELECT COUNT(Epoch) as Num_epochs, Mode, Representation, SUM(Time_in_sec) AS Total_Time, MAX(Accuracy) AS Max_ACC, MAX(F1_Score) AS Max_F1 FROM df GROUP BY Mode, Representation ORDER BY Mode, Representation, Max_F1 DESC, Max_ACC DESC",     
+         
+        # Get each dataset total time 
+        "SELECT Mode, SUM(Time_in_sec) AS Total_Time_in_sec, SUM(Time_in_sec)/60 AS Total_Time_in_min, SUM(Time_in_sec)/3600 AS Total_Time_in_hr FROM df GROUP BY Mode",
+
+        # Get the Best results 
+        "SELECT Mode, MAX(Accuracy), Max(F1_Score) FROM df GROUP BY Mode",
+
+        "SELECT COUNT(Epoch) as Num_epochs, Mode, Representation, SUM(Time_in_sec) AS Total_Time, MAX(Accuracy) AS Max_ACC, MAX(F1_Score) AS Max_F1 FROM df WHERE Epoch = 30 GROUP BY Mode, Representation ORDER BY Mode, Representation, Max_F1 DESC, Max_ACC DESC",     
+
+
+        # "SELECT * FROM df WHERE Mode ='Coarse' AND Accuracy >= 79.56 ",
+
+        # "SELECT * FROM df WHERE Mode ='Coarse' AND F1_Score >= 70.5 ",
+
+        # "SELECT * FROM df WHERE Mode ='Fine' AND Accuracy >= 73.67 ",
+
+        # "SELECT * FROM df WHERE Mode ='Fine' AND F1_Score >= 52.71 "
     ]
+
 
     for query in queries:
         print(duckdb.query(query).to_df())
-    
+        print("")
+
+def creater_charts(df):
+    for mode in MODES:
+        for representation in REPRESENTATIONS:
+            query = "SELECT * FROM df WHERE Mode = '%s' AND Representation = '%s'" % (mode, representation)
+            df_temp = duckdb.query(query).to_df()
+            df_temp.plot(x="Epoch", y = ["Accuracy", "F1_Score"], grid = True)
+            plt.savefig("IMG/%s_%s.png" % (mode, representation ))
+            plt.close()
+
+def build_excel(df):
+    for mode in MODES:
+        df_mode = duckdb.query("SELECT * FROM df WHERE Mode = '%s'" % (mode)).to_df()
+        df_mode.to_excel("Excel/1-MODE_%s_GermEval2018.xlsx" % (mode))    
+        del df_mode
+        for representation in REPRESENTATIONS:
+            query = "SELECT * FROM df WHERE Mode = '%s' AND Representation = '%s'" % (mode, representation)
+            df_temp = duckdb.query(query).to_df()
+            df_temp.to_excel("Excel/GermEval2018_%s_%s.xlsx" % (mode, representation ))    
+
+
 def main():
 
     data = []
-    for experiment in TEST:
+    for experiment in ALL:
         content = get_content(experiment)
-        epochs = content.split("\n")[12:]
+        epochs = content.split("\n")[12:-1]
         mode = content.split("\n")[0].split(",")[1].split("-")[1]
         representation = content.split("\n")[0].split(",")[1].split("-")[2]
             
@@ -104,7 +130,8 @@ def main():
     df = pd.DataFrame(data=data, columns=HEADER)
 
     statistics(df)
-    # df.to_excel("GermEval2018.xlsx") 
+    # creater_charts(df)
+    # build_excel(df)
 
 if __name__ == "__main__":
     main() 
